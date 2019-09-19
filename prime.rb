b26cec4773c9bb3ce21f132889a164a7aa1276af
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-19 06:35:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,14 @@
 # Add  code here!
 def prime?(num)
+if num < 0
+return false
+end
   n = 2
   while n < num
     return false if num % n == 0
     n += 1
   end
   true
-  if
-   num < 0
-  false
-  end
-  end
+end
 
 prime?(5)
